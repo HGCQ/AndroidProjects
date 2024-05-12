2e4327359482eab0101ffe5e422b17e23b11339d
Mangjun <myoungjun55@naver.com>
2024-05-12 22:37:57 +0900

    Add : 사진 업로드, 삭제 기능

--- photobook/View/app/src/main/java/hgcq/model/dto/PhotoDTO.java
+++ photobook/View/app/src/main/java/hgcq/model/dto/PhotoDTO.java
@@ -1,4 +1,47 @@
 package hgcq.model.dto;
 
-public class PhotoDTO {
+import java.io.Serializable;
+import java.time.LocalDate;
+
+public class PhotoDTO implements Serializable {
+
+    private String imageName;
+
+    private byte[] image;
+
+    private String date;
+
+    public PhotoDTO() {
+    }
+
+    public PhotoDTO(String imageName, String date) {
+        this.imageName = imageName;
+        this.date = date;
+    }
+
+    public PhotoDTO(String imageName, byte[] image, String date) {
+        this.imageName = imageName;
+        this.image = image;
+        this.date = date;
+    }
+
+    public String getImageName() {
+        return imageName;
+    }
+
+    public byte[] getImage() {
+        return image;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    @Override
+    public String toString() {
+        return "PhotoDTO{" +
+                "imageName='" + imageName + '\'' +
+                ", date=" + date +
+                '}';
+    }
 }
