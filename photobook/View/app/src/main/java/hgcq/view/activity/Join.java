4d977e72651e95c3e06f9b55fc1d128c5e94f88c
hs010304 <yyhhss0304@naver.com>
2024-05-12 17:18:05 +0900

    Add : 로그인, 회원가입 기능 및 레이아웃

--- photobook/View/app/src/main/java/hgcq/view/activity/Join.java
+++ photobook/View/app/src/main/java/hgcq/view/activity/Join.java
@@ -1,6 +1,9 @@
 package hgcq.view.activity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.Spinner;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -16,5 +19,21 @@ public class Join extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_join);
+
+        Spinner spinner = findViewById(R.id.spinner);
+
+        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                // 스피너 아이템 선택 시 동작 구현
+                String selectedItem = parent.getItemAtPosition(position).toString();
+                // ...
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+                // 아무것도 선택되지 않았을 때 동작 구현
+            }
+        });
     }
 }
--- photobook/View/app/src/main/java/hgcq/view/activity/Login.java
+++ photobook/View/app/src/main/java/hgcq/view/activity/Login.java
@@ -1,6 +1,11 @@
 package hgcq.view.activity;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,13 +13,46 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.google.android.material.textfield.TextInputLayout;
+
 import hgcq.view.R;
 
 public class Login extends AppCompatActivity {
+    private EditText usernameEditText;
+    private EditText passwordEditText;
+    private Button loginButton;
 
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        usernameEditText = findViewById(R.id.editID);
+        passwordEditText = findViewById(R.id.editPassword);
+        loginButton = findViewById(R.id.loginbutton);
+        TextView passwordFindTextView = findViewById(R.id.FindPwd);
+        TextView signUpTextView = findViewById(R.id.Join);
+
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String username = usernameEditText.getText().toString();
+                String password = passwordEditText.getText().toString();
+            // 로그인
+                passwordFindTextView.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        // 비밀번호 찾기 기능 구현
+                    }
+                });
+                signUpTextView.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        // 회원가입 기능 구현
+                    }
+                });
+            }
+        });
     }
 }
