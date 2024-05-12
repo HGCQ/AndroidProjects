bebc3a3dba8b1ab86a5085f4c2888f77acb76559
LEEDDONG <ehdgus2580@naver.com>
2024-05-12 23:37:32 +0900

    Add : member 회원가입 로그인  정보 수정 기능

--- photobook/View/app/build.gradle.kts
+++ photobook/View/app/build.gradle.kts
@@ -8,7 +8,7 @@ android {
 
     defaultConfig {
         applicationId = "hgcq.view"
-        minSdk = 24
+        minSdk = 26
         targetSdk = 34
         versionCode = 1
         versionName = "1.0"
@@ -32,6 +32,17 @@ android {
 }
 
 dependencies {
+
+    implementation ("com.squareup.retrofit2:retrofit:2.9.0")
+    implementation ("com.squareup.retrofit2:converter-gson:2.9.0")
+    implementation ("com.squareup.okhttp3:okhttp:4.9.0")
+    implementation ("com.squareup.okhttp3:okhttp-urlconnection:4.9.0")
+    implementation ("com.squareup.okhttp3:logging-interceptor:4.9.0")
+
+    implementation ("com.github.bumptech.glide:glide:4.12.0")
+    implementation ("com.github.bumptech.glide:compiler:4.12.0")
+
+
 
     implementation(libs.appcompat)
     implementation(libs.material)
