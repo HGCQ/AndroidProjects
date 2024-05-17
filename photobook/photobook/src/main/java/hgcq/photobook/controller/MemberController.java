ead896ec330d646cb6d3e6afc9042f51624fe6af
Mangjun <myoungjun55@naver.com>
2024-05-17 11:09:50 +0900

    Add : 회원 친구 관련 로직 및 Rest API 개발

--- photobook/photobook/src/main/java/hgcq/photobook/controller/MemberController.java
+++ photobook/photobook/src/main/java/hgcq/photobook/controller/MemberController.java
@@ -12,6 +12,9 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Controller
 @RequiredArgsConstructor
 @RequestMapping("/member")
@@ -19,6 +22,7 @@ public class MemberController {
 
     private final MemberService memberService;
 
+    // 테스트 완료
     @PostMapping("/join")
     public ResponseEntity<?> join(@RequestBody MemberDTO memberDTO, HttpServletResponse response) {
         Member member = new Member(memberDTO.getName(), memberDTO.getEmail(), memberDTO.getPassword());
@@ -27,6 +31,7 @@ public class MemberController {
         return ResponseEntity.ok(id);
     }
 
+    // 테스트 완료
     @PostMapping("/login")
     public ResponseEntity<?> login(@RequestBody MemberDTO member,
                                    HttpServletResponse response, HttpServletRequest request) {
@@ -40,23 +45,134 @@ public class MemberController {
 
             return ResponseEntity.ok(loginMemberDTO);
         } else {
-            return ResponseEntity.status(HttpStatus.FORBIDDEN).body("login fail");
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("로그인 실패");
         }
     }
 
+    // 테스트 완료
+    @PostMapping("/logout")
+    public ResponseEntity<?> logout(HttpServletRequest request) {
+        HttpSession session = request.getSession(false);
+
+        if (session != null) {
+            session.invalidate();
+            return ResponseEntity.ok("로그아웃 성공");
+        } else {
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("로그인 중이 아닙니다.");
+        }
+    }
+
+    // 테스트 완료
     @PostMapping("/update")
     public ResponseEntity<?> modify(@RequestBody MemberDTO memberDTO, HttpServletRequest request) {
         HttpSession session = request.getSession(false);
+
         if (session != null) {
             MemberDTO existingMember = (MemberDTO) session.getAttribute("member");
+
             if (existingMember != null) {
-                Member findMember = memberService.findOne(memberDTO.getEmail());
-                findMember.setName(memberDTO.getName());
-                findMember.setPassword(memberDTO.getPassword());
+                Member findMember = memberService.findOne(existingMember.getEmail());
+                if (memberDTO.getName() != null) {
+                    findMember.setName(memberDTO.getName());
+                }
+                if (memberDTO.getPassword() != null) {
+                    findMember.setPassword(memberDTO.getPassword());
+                }
                 memberService.update(findMember);
-                return ResponseEntity.ok("update success");
+                return ResponseEntity.ok("회원 정보 수정 성공");
             }
         }
-        return ResponseEntity.status(HttpStatus.FORBIDDEN).body("update fail");
+        return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("회원 정보 수정 실패");
+    }
+
+    // 테스트 완료
+    @PostMapping("/friend/add")
+    public ResponseEntity<?> addFriend(@RequestBody MemberDTO memberDTO, HttpServletRequest request) {
+        HttpSession session = request.getSession(false);
+
+        if (session != null) {
+            MemberDTO existingMember = (MemberDTO) session.getAttribute("member");
+
+            if (existingMember != null) {
+                Member findMember = memberService.findOne(existingMember.getEmail());
+
+                if (findMember != null) {
+                    Member friend = memberService.findOne(memberDTO.getEmail());
+
+                    if (friend != null) {
+                        memberService.addFriend(findMember, friend);
+                        return ResponseEntity.ok("친구 추가 성공");
+                    }
+                }
+            }
+        }
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body("친구 추가 실패");
+    }
+
+    // 테스트 완료
+    @PostMapping("/friend/delete")
+    public ResponseEntity<?> deleteFriend(@RequestBody MemberDTO memberDTO, HttpServletRequest request) {
+        HttpSession session = request.getSession(false);
+
+        if (session != null) {
+            MemberDTO existingMember = (MemberDTO) session.getAttribute("member");
+
+            if (existingMember != null) {
+                Member findMember = memberService.findOne(existingMember.getEmail());
+
+                if (findMember != null) {
+                    Member friend = memberService.findOne(memberDTO.getEmail());
+
+                    if (friend != null) {
+                        memberService.deleteFriend(findMember, friend);
+                        return ResponseEntity.ok("친구 삭제 성공");
+                    }
+                }
+            }
+        }
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body("친구 삭제 실패");
+    }
+
+    // 테스트 완료
+    @GetMapping("/friend/list")
+    public ResponseEntity<?> listFriends(HttpServletRequest request) {
+        HttpSession session = request.getSession(false);
+
+        if (session != null) {
+            MemberDTO existingMember = (MemberDTO) session.getAttribute("member");
+
+            if (existingMember != null) {
+                Member findMember = memberService.findOne(existingMember.getEmail());
+
+                if (findMember != null) {
+                    List<Member> friends = memberService.friends(findMember);
+                    List<MemberDTO> memberDTOS = parseMemberDTOList(friends);
+                    return ResponseEntity.ok(memberDTOS);
+                }
+            }
+        }
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body("친구 리스트 조회 실패");
+    }
+
+    @ExceptionHandler(IllegalArgumentException.class)
+    private ResponseEntity<String> handleIllegalArgumentException(IllegalArgumentException e) {
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getMessage());
+    }
+
+    private MemberDTO parseMemberDTO(Member member) {
+        MemberDTO memberDTO = new MemberDTO();
+
+        memberDTO.setEmail(member.getEmail());
+        memberDTO.setName(member.getName());
+
+        return memberDTO;
+    }
+
+    private List<MemberDTO> parseMemberDTOList(List<Member> members) {
+        List<MemberDTO> memberDTOList = new ArrayList<>();
+        for (Member member : members) {
+            memberDTOList.add(parseMemberDTO(member));
+        }
+        return memberDTOList;
     }
 }
