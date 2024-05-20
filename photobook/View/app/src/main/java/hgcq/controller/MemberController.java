d6b29f19366dc9f715b08122c3539cf0b5f2d4e8
Mangjun <myoungjun55@naver.com>
2024-05-20 13:31:41 +0900

    Add : 네트워크 설정 파일 추가 및 기존 코드 리펙터링

--- photobook/View/app/src/main/java/hgcq/controller/MemberController.java
+++ photobook/View/app/src/main/java/hgcq/controller/MemberController.java
@@ -13,7 +13,9 @@ import com.franmontiel.persistentcookiejar.persistence.SharedPrefsCookiePersisto
 import java.util.List;
 import java.util.concurrent.TimeUnit;
 
+import hgcq.callback.LoginCallback;
 import hgcq.callback.MemberCallback;
+import hgcq.config.NetworkClient;
 import hgcq.model.dto.MemberDTO;
 import hgcq.model.service.MemberService;
 import hgcq.model.service.PhotoService;
@@ -30,42 +32,14 @@ public class MemberController {
     private MemberService memberService;
     private Context context;
 
-    private final String serverIp = ""; // 서버 주소
-
 
     public MemberController(Context context) {
+        NetworkClient client = NetworkClient.getInstance(context.getApplicationContext());
+        memberService = client.getMemberService();
         this.context = context;
-        // 쿠키 생성
-        ClearableCookieJar cookieJar =
-                new PersistentCookieJar(new SetCookieCache(), new SharedPrefsCookiePersistor(context));
-
-        // Http 메시지 로그
-        HttpLoggingInterceptor logging = new HttpLoggingInterceptor()
-                .setLevel(HttpLoggingInterceptor.Level.BODY);
-
-        // Http 커넥션 설정
-        OkHttpClient okHttpClient = new OkHttpClient.Builder()
-                .cookieJar(cookieJar)
-                .addInterceptor(logging)
-                .connectTimeout(30, TimeUnit.SECONDS)
-                .readTimeout(30, TimeUnit.SECONDS)
-                .writeTimeout(30, TimeUnit.SECONDS)
-                .build();
-
-        // 서버와 연결
-        Retrofit retrofit = new Retrofit.Builder()
-                .baseUrl(serverIp)
-                .client(okHttpClient)
-                .addConverterFactory(GsonConverterFactory.create())
-                .build();
-
-        memberService = retrofit.create(MemberService.class);
-
-
     }
 
     public void createMember(MemberDTO memberDto) {
-        //
         Call<ResponseBody> call = memberService.joinMember(memberDto);
         call.enqueue(new Callback<ResponseBody>() {
             @Override
@@ -73,12 +47,10 @@ public class MemberController {
                 if (response.isSuccessful()) {
                     Toast.makeText(context, "회원 가입 성공!", Toast.LENGTH_SHORT).show();
                     Log.d("회원 가입 성공!", "성공" + response.code());
-
                 } else {
                     Toast.makeText(context, "회원 가입 실패!", Toast.LENGTH_SHORT).show();
                     Log.e("회원 가입 실패!", "에러:" + response.code());
                 }
-
             }
 
             @Override
@@ -89,8 +61,7 @@ public class MemberController {
         });
     }
 
-
-    public void loginMember(MemberDTO memberDto) {
+    public void loginMember(MemberDTO memberDto, LoginCallback callback) {
         Call<ResponseBody> call = memberService.loginMember(memberDto);
         call.enqueue(new Callback<ResponseBody>() {
             @Override
@@ -98,18 +69,19 @@ public class MemberController {
                 if (response.isSuccessful()) {
                     Toast.makeText(context, "로그인 성공!", Toast.LENGTH_SHORT).show();
                     Log.d("로그인 성공!", "성공" + response.code());
-
+                    callback.onSuccess(true);
                 } else {
                     Toast.makeText(context, "로그인 실패ㅜㅜ", Toast.LENGTH_SHORT).show();
                     Log.e("로그인 실패ㅜㅜ", "에러:" + response.code());
+                    callback.onSuccess(false);
                 }
-
             }
 
             @Override
             public void onFailure(Call<ResponseBody> call, Throwable t) {
                 Toast.makeText(context, "사진 업로드 성공", Toast.LENGTH_SHORT).show();
                 Log.e("로그인:서버에서 응답받지 못했어용ㅜㅜ", t.getMessage());
+                callback.onError(t.getMessage());
             }
         });
     }
@@ -123,17 +95,14 @@ public class MemberController {
                 if (response.isSuccessful()) {
                     Toast.makeText(context, "회원 정보 수정 완료!", Toast.LENGTH_SHORT).show();
                     Log.d("회원 정보 수정 완료!", "성공" + response.code());
-
                 } else {
                     Toast.makeText(context, "회원 정보 수정 실패 ㅜㅜ", Toast.LENGTH_SHORT).show();
                     Log.e("회원 정보 수정 실패 ㅜㅜ", "에러:" + response.code());
                 }
-
             }
 
             @Override
             public void onFailure(Call<ResponseBody> call, Throwable t) {
-
                 Toast.makeText(context, "회원 정보 수정:서버에서 응답받지 못했어용", Toast.LENGTH_SHORT).show();
                 Log.e("회원 정보 수정:서버에서 응답받지 못했어용", t.getMessage());
             }
@@ -141,7 +110,6 @@ public class MemberController {
     }
 
     public void logoutMember(MemberDTO memberDto) {
-        //
         Call<ResponseBody> call = memberService.logoutMember(memberDto);
         call.enqueue(new Callback<ResponseBody>() {
             @Override
@@ -149,12 +117,10 @@ public class MemberController {
                 if (response.isSuccessful()) {
                     Toast.makeText(context, "로그 아웃 성공!", Toast.LENGTH_SHORT).show();
                     Log.d("로그 아웃 성공!", "성공" + response.code());
-
                 } else {
                     Toast.makeText(context, "로그아웃  실패!", Toast.LENGTH_SHORT).show();
                     Log.e("로그아웃  실패!", "에러:" + response.code());
                 }
-
             }
 
             @Override
@@ -174,12 +140,10 @@ public class MemberController {
                 if (response.isSuccessful()) {
                     Toast.makeText(context, "친구 추가 성공!", Toast.LENGTH_SHORT).show();
                     Log.d("친구 추가 성공!", "성공" + response.code());
-
                 } else {
                     Toast.makeText(context, "친구 추가 실패!", Toast.LENGTH_SHORT).show();
                     Log.e("친구 추가 실패!", "에러:" + response.code());
                 }
-
             }
 
             @Override
@@ -191,7 +155,6 @@ public class MemberController {
     }
 
     public void deleteFriend(MemberDTO memberDto) {
-        //
         Call<ResponseBody> call = memberService.deleteFriend(memberDto);
         call.enqueue(new Callback<ResponseBody>() {
             @Override
@@ -199,12 +162,10 @@ public class MemberController {
                 if (response.isSuccessful()) {
                     Toast.makeText(context, "친구 삭제 성공!", Toast.LENGTH_SHORT).show();
                     Log.d("친구 삭제 성공!", "성공" + response.code());
-
                 } else {
                     Toast.makeText(context, "친구 삭제 실패!", Toast.LENGTH_SHORT).show();
                     Log.e("친구 삭제 실패!", "에러:" + response.code());
                 }
-
             }
 
             @Override
@@ -228,7 +189,6 @@ public class MemberController {
                     Log.e("친구리스트 조회 실패", "에러:" + response.code());
                     callback.onError("에러:" + response.code());
                 }
-
             }
 
             @Override
