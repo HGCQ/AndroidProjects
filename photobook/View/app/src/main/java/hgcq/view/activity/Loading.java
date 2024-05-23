3ae41126e68448ef312139cdf9e2e254d275eef3
Mangjun <myoungjun55@naver.com>
2024-05-23 11:54:34 +0900

    JSY / Add : UI 디자인 및 로딩중 액티비티 기능 추가

--- photobook/View/app/src/main/java/hgcq/view/activity/Loading.java
+++ photobook/View/app/src/main/java/hgcq/view/activity/Loading.java
@@ -1,17 +1,22 @@
 package hgcq.view.activity;
 
+import android.content.Intent;
 import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ImageView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.bumptech.glide.Glide;
 
+import hgcq.config.NetworkClient;
 import hgcq.view.R;
 
 public class Loading extends AppCompatActivity {
-    private ImageView imageView2;
+
+    private NetworkClient client;
+    private ImageView loading;
     private MediaPlayer mediaPlayer;
 
     @Override
@@ -19,22 +24,31 @@ public class Loading extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_loading);
 
-        imageView2 = findViewById(R.id.imageView2);
+        client = NetworkClient.getInstance(this);
+
+        loading = findViewById(R.id.loading);
         Glide.with(this)
                 .asGif()
                 .load(R.drawable.loading)
-                .into(imageView2);
+                .into(loading);
+
+        Intent login = new Intent(this, Login.class);
+        Intent main = new Intent(this, Main.class);
+
+        loading.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mediaPlayer.stop();
+                if (client.isLogin()) {
+                    startActivity(main);
+                } else {
+                    startActivity(login);
+                }
+            }
+        });
 
         // MP3 파일 자동 재생
         mediaPlayer = MediaPlayer.create(this, R.raw.loading);
         mediaPlayer.start(); // 음악 자동 재생
-    }
-
-    @Override
-    protected void onDestroy() {
-        super.onDestroy();
-        if (mediaPlayer != null) {
-            mediaPlayer.release();
-        }
     }
 }
