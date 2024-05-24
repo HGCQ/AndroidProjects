931a01002ffc54975cc5058b33adc146dd2e3849
Mangjun <myoungjun55@naver.com>
2024-05-25 00:09:56 +0900

    Modfiy : 중복 검사 및 날짜 오름차순으로 수정

--- photobook/photobook/src/main/java/hgcq/photobook/repository/EventMemberRepository.java
+++ photobook/photobook/src/main/java/hgcq/photobook/repository/EventMemberRepository.java
@@ -84,7 +84,7 @@ public class EventMemberRepository {
      * @return 이벤트 리스트
      */
     public List<Event> findEventByName(String name, Member member) {
-        return em.createQuery("select em.event from EventMember em where em.event.name LIKE :name and em.member = :member", Event.class)
+        return em.createQuery("select em.event from EventMember em where em.event.name LIKE :name and em.member = :member order by em.event.date", Event.class)
                 .setParameter("name", "%" + name + "%")
                 .setParameter("member", member)
                 .getResultList();
@@ -138,7 +138,7 @@ public class EventMemberRepository {
      * @return 이벤트 리스트
      */
     public List<Event> findEvents(Member member) {
-        return em.createQuery("select em.event from EventMember em where em.member = :member", Event.class)
+        return em.createQuery("select em.event from EventMember em where em.member = :member order by em.event.date", Event.class)
                 .setParameter("member", member)
                 .getResultList();
     }
@@ -150,7 +150,7 @@ public class EventMemberRepository {
      * @return 이벤트 날짜 리스트
      */
     public List<LocalDate> findEventsToDate(Member member) {
-        return em.createQuery("select em.event.date from EventMember em where em.member = :member", LocalDate.class)
+        return em.createQuery("select em.event.date from EventMember em where em.member = :member order by em.event.date", LocalDate.class)
                 .setParameter("member", member)
                 .getResultList();
     }
