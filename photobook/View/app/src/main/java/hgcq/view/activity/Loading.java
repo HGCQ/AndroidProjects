950702ad15c34b7728aaaffd09ca3d6c28ed28f9
서윤 <choseoyung@naver.com>
2024-05-29 16:09:42 +0900

    Modify : 전체적인 UI 디자인 수정

--- photobook/View/app/src/main/java/hgcq/view/activity/Loading.java
+++ photobook/View/app/src/main/java/hgcq/view/activity/Loading.java
@@ -23,7 +23,9 @@ public class Loading extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_loading);
-
+        if (getSupportActionBar() != null) {
+            getSupportActionBar().hide();
+        }
         client = NetworkClient.getInstance(this);
 
         loading = findViewById(R.id.loading);
